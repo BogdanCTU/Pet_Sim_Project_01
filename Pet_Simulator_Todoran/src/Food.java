267f88266dc588a889d00f57e92b7e808db2ab30
bctod <Bogdan200!>
2021-10-01 19:14:39 +0200

    Project created/

--- Pet_Simulator_Todoran/src/Food.java
+++ Pet_Simulator_Todoran/src/Food.java
@@ -1,3 +1,30 @@
 public class Food {
-    //hello
+
+    // variables
+    public String food_Name;
+    public int pet_Height;
+    public int pet_Weight;
+    private int food_Price;
+    private int pet_Water;
+    private int pet_Food;
+
+    // constructor
+    public Pet(String proprietar, String locatie, int area, int sold) {
+        this.pet_Name = pet_Name;
+        this.pet_Height = pet_Height;
+        this.pet_Weight = pet_Weight;
+        this.pet_Wealth = pet_Wealth;
+        this.pet_Water = pet_Water;
+        this.pet_Food = pet_Food;
+    }
+
+    // get / set
+    public void setPet_Wealth (int Wealth) {this.pet_Wealth = Wealth; };
+    public void setPet_Water (int Water) {this.pet_Water = Water; };
+    public void setPet_Food (int Food) {this.pet_Food = Food; };
+
+    public int getPet_Wealth () {return this.pet_Wealth; };
+    public int getPet_Water () {return this.pet_Water; };
+    public int getPet_Foodr () {return this.pet_Food; };
+
 }

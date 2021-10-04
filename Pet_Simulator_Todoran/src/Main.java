b434bc3c7bb5a38717f754a41484c971976deb15
bctod <Bogdan200!>
2021-10-04 16:35:38 +0200

    Project created/

--- Pet_Simulator_Todoran/src/Main.java
+++ Pet_Simulator_Todoran/src/Main.java
@@ -27,7 +27,7 @@ public class Main {
         insert_Data = Metode.GetInsertDataAnswer();
 
         //compare string
-        if((Objects.equals(insert_Data, "yes"))){
+        if ((Objects.equals(insert_Data, "yes"))) {
             // User Data
             Metode.GetMoney(money);
             pets = Metode.GetPetsNum();
@@ -36,195 +36,91 @@ public class Main {
             my_Pets = Metode.GetPetsData(my_Pets, pets);
 
             //simulator
-            while(!game_Over){
+            while (!game_Over) {
                 // console clear command?!?!?
 
                 // Console Simulator
                 Metode.DayConsole(day);
 
-                //decrease health conditions
-                for (int i = 0; i < pets; ++i) {
-                    my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() - 25);
-                    my_Pets[i].setPet_Food(my_Pets[i].getPet_Food() - 25);
-                    my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() - 10);
-                    if(my_Pets[i].getPet_Water() <= 0 && my_Pets[i].getPet_Food() <= 0) my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() - 10);
-                }
+                // Decrease Health Conditions
+                Metode.DecreaseHealthConditions(my_Pets, pets);
 
                 //simulating
-                for (int i = 0; i < pets; ++i) {
-                    if (my_Pets[i].getPet_Water() >= 25 && my_Pets[i].getPet_Water() <= 50 && my_Pets[i].pet_Life == 1)
-                        System.out.println(my_Pets[i].pet_Name + " needs some water!");
-                    else if (my_Pets[i].getPet_Water() <= 0 && my_Pets[i].getPet_Water() < 25 && my_Pets[i].pet_Life == 1)
-                        System.out.println(my_Pets[i].pet_Name + " is thirsty!!!");
-                    if (my_Pets[i].getPet_Water() <= 50) {
-                        if (money >= 0) {
-                            String answer_Water;
-                            System.out.print("Would you like to buy " + my_Pets[i].pet_Name + " some water? (yes / no) - ");
-                            answer_Water = sc.next();
-                            if ((Objects.equals(answer_Water, "yes"))) {
-                                int answer_Water_nr;
-                                System.out.print("You can buy : \n 1 - Water \n 2 - Milk \n Wich one would you like to buy? ( 1 / 2 ) - ");
-                                answer_Water_nr = sc.nextInt();
-                                answer_Water_nr--;
-                                drinks[answer_Water_nr].setfood_Quantity(drinks[answer_Water_nr].getfood_Quantity() + 1);
-                                money -= drinks[answer_Water_nr].getfood_Price();
-                                my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() + drinks[answer_Water_nr].food_Heal);
-                                System.out.println(my_Pets[i].pet_Name + " just drinked some " + drinks[answer_Water_nr].food_Name);
-                            }
-                        }
-                    }
-                    if(my_Pets[i].getPet_Food() >= 25 && my_Pets[i].getPet_Food() <= 50 && my_Pets[i].pet_Life == 1)
-                        System.out.println(my_Pets[i].pet_Name + " needs some food!");
-                    else if(my_Pets[i].getPet_Food() <= 0 && my_Pets[i].getPet_Food() < 25 && my_Pets[i].pet_Life == 1)
-                        System.out.println(my_Pets[i].pet_Name + " is hungry!!!");
-                    if (my_Pets[i].getPet_Food() <= 50) {
-                        if (money >= 0) {
-                            String answer_Food;
-                            System.out.print("Would you like to buy " + my_Pets[i].pet_Name + " some food? (yes / no) - ");
-                            answer_Food = sc.next();
-                            if ((Objects.equals(answer_Food, "yes"))) {
-                                int answer_food_nr;
-                                System.out.print("You can buy : \n 1 - Meal \n 2 - Fish \n 3 - Soup \n Wich one would you like to buy? ( 1 / 2 / 3 ) - ");
-                                answer_food_nr = sc.nextInt();
-                                answer_food_nr--;
-                                foods[answer_food_nr].setfood_Quantity(foods[answer_food_nr].getfood_Quantity() + 1);
-                                money -= foods[answer_food_nr].getfood_Price();
-                                my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() + foods[answer_food_nr].food_Heal);
-                                System.out.println(my_Pets[i].pet_Name + " just eated some " + foods[answer_food_nr].food_Name);
-                            }
-                        }
-                    }
-                    if (my_Pets[i].getPet_Food() >= 25 && my_Pets[i].getPet_Water() >= 25)
-                        my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() + 20);
+                //Metode.SimulatePets(my_Pets, pets);
 
+                for (int i = 0; i < pets; ++i) {
+                    // Pet Water Need
+                    Metode.PetWaterNeed(my_Pets, i);
+                    Metode.PetWaterManual(money, drinks, my_Pets, i);
+
+                    // Pet Food Need
+                    Metode.PetFoodNeed(my_Pets, i);
+                    Metode.PetFoodManual(money, foods, my_Pets, i);
+
+                    //Pet Health Regen
+                    Metode.PetHealthRegen(my_Pets, i);
                 }
 
                 //lose condition
-                int temp = 0;
-                for (int i = 0; i < pets; ++i) {
-                    if(my_Pets[i].getPet_Wealth() <= 0) {
-                        my_Pets[i].setPet_Water(0);
-                        if(my_Pets[i].pet_Life == 1) System.out.println(my_Pets[i].pet_Name + " just died!!!");
-                        my_Pets[i].pet_Life = 0;
-                        temp++;
-                    }
-                }
-                if (temp == pets) game_Over = true;
+                game_Over = Metode.LoseCondition(my_Pets, pets, game_Over);
 
-                //next day starter
+                //Next Day
                 System.out.println("You have " + money + " money left!");
                 temp_Day++;
                 day++;
-                if(temp_Day == 7){
+                if (temp_Day == 7) {
                     temp_Day = 1;
                     money += 100;
                 }
             }
-        }
-        else{
+        } else {
             Metode.GetMoney(money);
             System.out.println("----------------------------------------------------------");
             pets = rnd.nextInt(5);
-            if(pets < 1) pets = 1;
+            if (pets < 1) pets = 1;
 
-            if(pets == 1) System.out.println("You have one pet!");
+            if (pets == 1) System.out.println("You have one pet!");
             else System.out.println("You have " + pets + " pets!");
 
             // Generate Pets Data
-            my_Pets = Metode.GeneratePetsData(my_Pets, pets);
+            Metode.GeneratePetsData(my_Pets, pets);
 
-            //simulator
-            while(!game_Over){
-                /*
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-                getConsoleView().clear();
-                console.clear();
-                verify(consoleView).clear();
-                */
-                System.out.println("----------------------------------------------------------");
-                System.out.println("Day nr'" + day);
-                System.out.print("Start simulating");
-                for(int i = 0; i < 3; ++i) {
-                    java.util.concurrent.TimeUnit.SECONDS.sleep(1);
-                    System.out.print(".");
-                    //Thread.sleep(1000);
-                }
-                System.out.print("\n\n");
+            // Simulator
+            while (!game_Over) {
+                // Console Simulator
+                Metode.DayConsole(day);
 
-                //decrease health conditions
-                for (int i = 0; i < pets; ++i) {
-                    my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() - 25);
-                    my_Pets[i].setPet_Food(my_Pets[i].getPet_Food() - 25);
-                    my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() - 10);
-                    if(my_Pets[i].getPet_Water() <= 0 && my_Pets[i].getPet_Food() <= 0) my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() - 10);
-                }
+                // Decrease Health Conditions
+                Metode.DecreaseHealthConditions(my_Pets, pets);
 
                 //simulating
                 for (int i = 0; i < pets; ++i) {
-                    if(my_Pets[i].getPet_Water() >= 25 && my_Pets[i].getPet_Water() <= 50 && my_Pets[i].pet_Life == 1) System.out.println(my_Pets[i].pet_Name + " needs some water!");
-                    else if(my_Pets[i].getPet_Water() <= 0 && my_Pets[i].getPet_Water() < 25 && my_Pets[i].pet_Life == 1) System.out.println(my_Pets[i].pet_Name + " is thirsty!!!");
-                    if (my_Pets[i].getPet_Water() <= 50) {
-                        if (money >= 0) {
-                            int prob = rnd.nextInt(99);
-                            if (prob <= 75) {
-                                int temp = rnd.nextInt(1);
-                                drinks[temp].setfood_Quantity(drinks[temp].getfood_Quantity() + 1);
-                                money -= drinks[temp].getfood_Price();
-                                my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() + drinks[temp].food_Heal);
-                                System.out.println(my_Pets[i].pet_Name + " just drinked some " + drinks[temp].food_Name);
-                            }
-                        }
-                    }
-                    if(my_Pets[i].getPet_Food() >= 25 && my_Pets[i].getPet_Food() <= 50 && my_Pets[i].pet_Life == 1) System.out.println(my_Pets[i].pet_Name + " needs some food!");
-                    else if(my_Pets[i].getPet_Food() <= 0 && my_Pets[i].getPet_Food() < 25 && my_Pets[i].pet_Life == 1) System.out.println(my_Pets[i].pet_Name + " is hungry!!!");
-                    if (my_Pets[i].getPet_Food() <= 50) {
-                        if (money >= 0) {
-                            int prob = rnd.nextInt(99);
-                            if (prob <= 50) {
-                                int temp = rnd.nextInt(2);
-                                foods[temp].setfood_Quantity(drinks[temp].getfood_Quantity() + 1);
-                                money -= foods[temp].getfood_Price();
-                                my_Pets[i].setPet_Food(my_Pets[i].getPet_Food() + foods[temp].food_Heal);
-                                System.out.println(my_Pets[i].pet_Name + " just eated some " + foods[temp].food_Name);
-                            }
-                        }
-                    }
-                    if (my_Pets[i].getPet_Food() >= 25 && my_Pets[i].getPet_Water() >= 25)
-                        my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() + 20);
+                    // Pet Water Need
+                    Metode.PetWaterNeed(my_Pets, i);
+                    Metode.PetWaterProbability(money, drinks, my_Pets, i);
+
+                    // Pet Food Need
+                    Metode.PetFoodNeed(my_Pets, i);
+                    Metode.PetFoodProbability(money, foods, my_Pets, i);
+
+                    //Pet Health Regen
+                    Metode.PetHealthRegen(my_Pets, i);
                 }
 
                 //lose condition
-                int temp = 0;
-
-                for (int i = 0; i < pets; ++i) {
-                    if(my_Pets[i].getPet_Wealth() <= 0) {
-                        my_Pets[i].setPet_Water(0);
-                        if(my_Pets[i].pet_Life == 1) System.out.println(my_Pets[i].pet_Name + " just died!!!");
-                        my_Pets[i].pet_Life = 0;
-                        temp++;
-                    }
-                }
-                if (temp == pets) game_Over = true;
+                game_Over = Metode.LoseCondition(my_Pets, pets, game_Over);
 
                 System.out.println("You have " + money + " money left!");
                 day++;
                 temp_Day++;
-                if(temp_Day == 7){
+                if (temp_Day == 7) {
                     temp_Day = 1;
                     money += rnd.nextInt(100);
                 }
             }
         }
 
-        System.out.println("----------------------------------------------------------");
-        System.out.println("Your pets survived " + (day - 1) + " days!");
-
-        String lost[] = {"Y","o","u"," ","l","o","s","t","!"};
-        for(int i = 0; i < 9; ++i) {
-            Thread.sleep(300);
-            System.out.print(lost[i]);
-        }
+        //End Game
+        Metode.YouLost(day);
     }
-
 }
--- Pet_Simulator_Todoran/src/Metode.java
+++ Pet_Simulator_Todoran/src/Metode.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -79,9 +80,6 @@ public class Metode {
 
     public static Pet[] GeneratePetsData(Pet[] my_Pets, int pets) {
         System.out.println("----------------------------------------------------------");
-        System.out.println("Please insert pets informations ");
-
-        System.out.println("----------------------------------------------------------");
         System.out.println("Your pets are: ");
 
         for (int i = 0; i < pets; ++i) {
@@ -115,20 +113,131 @@ public class Metode {
         System.out.print("\n\n");
     }
 
-    /*
-    public static void (Pet my_Pets){
+    public static Pet[] DecreaseHealthConditions(Pet[] my_Pets, int pets) {
         for (int i = 0; i < pets; ++i) {
             my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() - 25);
             my_Pets[i].setPet_Food(my_Pets[i].getPet_Food() - 25);
             my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() - 10);
-            if(my_Pets[i].getPet_Water() <= 0 && my_Pets[i].getPet_Food() <= 0) my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() - 10);
+            if (my_Pets[i].getPet_Water() <= 0 && my_Pets[i].getPet_Food() <= 0)
+                my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() - 10);
+        }
+        return my_Pets;
+    }
+
+    public static void PetWaterNeed(Pet[] my_Pets,int i) {
+        if (my_Pets[i].getPet_Water() >= 25 && my_Pets[i].getPet_Water() <= 50 && my_Pets[i].pet_Life == 1)
+            System.out.println(my_Pets[i].pet_Name + " needs some water!");
+        else if (my_Pets[i].getPet_Water() <= 0 && my_Pets[i].getPet_Water() < 25 && my_Pets[i].pet_Life == 1)
+            System.out.println(my_Pets[i].pet_Name + " is thirsty!!!");
+    }
+
+    public static void PetWaterManual(int money, Food[] drinks, Pet[] my_Pets, int i) {
+        if (my_Pets[i].getPet_Water() <= 50) {
+            if (money >= 0) {
+                String answer_Water;
+                System.out.print("Would you like to buy " + my_Pets[i].pet_Name + " some water? (yes / no) - ");
+                answer_Water = sc.next();
+                if ((Objects.equals(answer_Water, "yes"))) {
+                    int answer_Water_nr;
+                    System.out.print("You can buy : \n 1 - Water \n 2 - Milk \n Wich one would you like to buy? ( 1 / 2 ) - ");
+                    answer_Water_nr = sc.nextInt();
+                    answer_Water_nr--;
+                    drinks[answer_Water_nr].setfood_Quantity(drinks[answer_Water_nr].getfood_Quantity() + 1);
+                    money -= drinks[answer_Water_nr].getfood_Price();
+                    my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() + drinks[answer_Water_nr].food_Heal);
+                    System.out.println(my_Pets[i].pet_Name + " just drinked some " + drinks[answer_Water_nr].food_Name);
+                }
+            }
         }
     }
-    */
 
+    public static void PetFoodNeed(Pet[] my_Pets,int i) {
+        if (my_Pets[i].getPet_Food() >= 25 && my_Pets[i].getPet_Food() <= 50 && my_Pets[i].pet_Life == 1)
+            System.out.println(my_Pets[i].pet_Name + " needs some food!");
+        else if (my_Pets[i].getPet_Food() <= 0 && my_Pets[i].getPet_Food() < 25 && my_Pets[i].pet_Life == 1)
+            System.out.println(my_Pets[i].pet_Name + " is hungry!!!");
+    }
 
+    public static void PetFoodManual(int money, Food[] foods, Pet[] my_Pets, int i) {
+        if (my_Pets[i].getPet_Food() <= 50) {
+            if (money >= 0) {
+                String answer_Food;
+                System.out.print("Would you like to buy " + my_Pets[i].pet_Name + " some food? (yes / no) - ");
+                answer_Food = sc.next();
+                if ((Objects.equals(answer_Food, "yes"))) {
+                    int answer_food_nr;
+                    System.out.print("You can buy : \n 1 - Meal \n 2 - Fish \n 3 - Soup \n Wich one would you like to buy? ( 1 / 2 / 3 ) - ");
+                    answer_food_nr = sc.nextInt();
+                    answer_food_nr--;
+                    foods[answer_food_nr].setfood_Quantity(foods[answer_food_nr].getfood_Quantity() + 1);
+                    money -= foods[answer_food_nr].getfood_Price();
+                    my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() + foods[answer_food_nr].food_Heal);
+                    System.out.println(my_Pets[i].pet_Name + " just eated some " + foods[answer_food_nr].food_Name);
+                }
+            }
+        }
+    }
 
+    public static void PetHealthRegen(Pet[] my_Pets, int i){
+        if (my_Pets[i].getPet_Food() >= 50 && my_Pets[i].getPet_Water() >= 50)
+            my_Pets[i].setPet_Wealth(my_Pets[i].getPet_Wealth() + 20);
+    }
 
+    public static boolean LoseCondition(Pet[] my_Pets, int pets, boolean game_Over) {
+        int temp = 0;
+        for (int i = 0; i < pets; ++i) {
+            if (my_Pets[i].getPet_Wealth() <= 0) {
+                my_Pets[i].setPet_Water(0);
+                if (my_Pets[i].pet_Life == 1) System.out.println(my_Pets[i].pet_Name + " just died!!!");
+                my_Pets[i].pet_Life = 0;
+                temp++;
+            }
+        }
+        if (temp == pets) game_Over = true;
+        else game_Over = false;
+        return game_Over;
+    }
 
+    public static void PetWaterProbability(int money, Food[] drinks, Pet[] my_Pets, int i) {
+        if (my_Pets[i].getPet_Water() <= 50) {
+            if (money >= 0) {
+                int prob = rnd.nextInt(99);
+                if (prob <= 75) {
+                    int temp = rnd.nextInt(1);
+                    drinks[temp].setfood_Quantity(drinks[temp].getfood_Quantity() + 1);
+                    money -= drinks[temp].getfood_Price();
+                    my_Pets[i].setPet_Water(my_Pets[i].getPet_Water() + drinks[temp].food_Heal);
+                    System.out.println(my_Pets[i].pet_Name + " just drinked some " + drinks[temp].food_Name);
+                }
+            }
+        }
+    }
 
+    public static void PetFoodProbability(int money, Food[] foods, Pet[] my_Pets, int i) {
+        if (my_Pets[i].getPet_Food() <= 50) {
+            if (money >= 0) {
+                int prob = rnd.nextInt(99);
+                if (prob <= 50) {
+                    int temp = rnd.nextInt(2);
+                    foods[temp].setfood_Quantity(foods[temp].getfood_Quantity() + 1);
+                    money -= foods[temp].getfood_Price();
+                    my_Pets[i].setPet_Food(my_Pets[i].getPet_Food() + foods[temp].food_Heal);
+                    System.out.println(my_Pets[i].pet_Name + " just eated some " + foods[temp].food_Name);
+                }
+            }
+        }
+    }
+
+    public static void YouLost(int day) throws InterruptedException {
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Your pets survived " + (day - 1) + " days!");
+
+        String lost[] = {"Y", "o", "u", " ", "l", "o", "s", "t", "!"};
+        for (int i = 0; i < 9; ++i) {
+            Thread.sleep(300);
+            System.out.print(lost[i]);
+        }
+    }
+
+    // END
 }
